4c64f1b8097f99802bb058feff6690f4451542f8
chika <>
2023-12-06 01:27:59 +0900

    20231205-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 
+
+
+const parentMenu = document.querySelectorAll(".l-header__menu li a");
+for (let i = 0; i < parentMenu.length; i++) {
+    parentMenu[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("active");
+    })
+}
+
+$('.l-slider ul').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    dotsClass: "slide-dots" //ここでclass名を変更する（デフォルトはslick-dots）
+});
